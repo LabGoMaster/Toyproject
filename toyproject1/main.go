16279527cdb24d1457548e76d198d0e223169b04
LabGoMaster <91514993+LabGoMaster@users.noreply.github.com>
2021-09-28 13:36:18 +0900

    Add files via upload
    원본

---
+++ toyproject1/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/antage/eventsource"
+	"github.com/gorilla/pat"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+//123
+var rd *render.Render
+var googleOauthConfig = oauth2.Config{
+	RedirectURL:  "http://localhost:3000/auth/google/callback",
+	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_SECRET_KEY"),
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
+}
+
+func googleLoginHandler(w http.ResponseWriter, r *http.Request) {
+	state := generateStateOauthCookie(w)
+	url := googleOauthConfig.AuthCodeURL(state)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	expiration := time.Now().Add(2 * 24 * time.Hour) //1로 하니깐 만료 됬음
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := &http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, cookie)
+	return state
+}
+
+func googleAuthCallback(w http.ResponseWriter, r *http.Request) {
+	rd.HTML(w, http.StatusOK, "hello", "hello2")
+
+	oauthstate, _ := r.Cookie("oauthstate")
+
+	if r.FormValue("state") != oauthstate.Value {
+		log.Printf("invalid google oauth state cookie:%s state:%s\n", oauthstate.Value, r.FormValue("state"))
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	data, err := getGoogleUserInfo(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Print(string(data))
+
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func getGoogleUserInfo(code string) ([]byte, error) {
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Exchange %s\n ", err.Error())
+	}
+
+	resp, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Get UserInfo %s\n ", err.Error())
+	}
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+//chat
+func postMessageHandler(w http.ResponseWriter, r *http.Request) {
+	msg := r.FormValue("msg")
+	name := r.FormValue("name")
+	//log.Println("postMEssageHandler ", msg, name)
+
+	es := eventsource.New(nil, nil)
+	defer es.Close()
+
+	sendMessage(name, msg)
+}
+
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("name")
+	sendMessage("", fmt.Sprintf("add user: %s", username))
+}
+
+func leftUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	sendMessage("", fmt.Sprintf("left user: %s", username))
+}
+
+type Message struct {
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
+}
+
+var msgCh chan Message
+
+func sendMessage(name, msg string) {
+	msgCh <- Message{name, msg}
+}
+
+func processMsgCh(es eventsource.EventSource) {
+	for msg := range msgCh {
+		data, _ := json.Marshal(msg)
+		es.SendEventMessage(string(data), "", strconv.Itoa(time.Now().Nanosecond()))
+
+	}
+}
+
+func main() {
+
+	rd = render.New(render.Options{
+		Directory:  "template",
+		Extensions: []string{".html", ".tmpl", ".css"},
+		Layout:     "hello",
+	})
+
+	//http.Handle("/hello/", http.StripPrefix("root/css/", http.FileServer(http.Dir("root/css"))))
+
+	msgCh = make(chan Message)
+	es := eventsource.New(nil, nil)
+	defer es.Close()
+
+	go processMsgCh(es)
+
+	mux := pat.New()
+
+	mux.Post("/messages", postMessageHandler)
+	mux.Handle("/stream", es)
+	mux.Post("/users", addUserHandler)
+	mux.Delete("/users", leftUserHandler)
+
+	mux.HandleFunc("/auth/google/login", googleLoginHandler)
+	mux.HandleFunc("/auth/google/callback", googleAuthCallback)
+
+	n := negroni.Classic()
+
+	n.UseHandler(mux)
+	http.ListenAndServe(":3000", n)
+}
